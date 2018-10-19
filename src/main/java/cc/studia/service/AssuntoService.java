9eb37dbda2dff4a0108f72da17f4e1df55622b1b
Andre Pfeiffer <andre@pfeiffer.com.br>
2018-10-18 23:47:07 -0300

    Update src/main/java/cc/studia/service/AssuntoService.java

--- src/main/java/cc/studia/service/AssuntoService.java
+++ src/main/java/cc/studia/service/AssuntoService.java
@@ -5,17 +5,10 @@ import java.util.List;
 import cc.studia.entity.Assunto;
 
 public interface AssuntoService {
-
 	List<Assunto> verTodos();
-
 	void salvarAssunto(Assunto assunto);
-
 	void editaAssunto(Assunto assunto);
-
 	Assunto ver(int id);
-
 	void removeAssunto(int id);
-
 	List<Assunto> busca(String busca);
-
 }
